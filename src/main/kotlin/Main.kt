b1ea03265bdf49ba6f1c0436e4684d0fc62eddfe
tomerdo <tomer.dobkin@gmail.com>
2022-05-07 13:13:48 +0300

    working with list of cidr notation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,15 @@
 import kotlin.math.pow
 
 data class IPGroup(val a: String, val b: String, val c: String, val d: String)
+
 const val IPV4_BITS = 32
 
 private const val SLASH_DELIMITER = '/'
 private const val DOT_DELIMITER = "."
 
 class IPFilter(cidrNotation: String) {
-    private val ipAllowedGroup: IPGroup
-    private val groupMask: Long
+    private val subnetAddress: IPGroup
+    private val subnetMask: Long
     private val addresses = 2.0.pow(IPV4_BITS)
     private val classSize = 2.0.pow(IPV4_BITS / 4)
 
@@ -16,8 +17,8 @@ class IPFilter(cidrNotation: String) {
         val (prefixAddress, suffix) = cidrNotation.split(SLASH_DELIMITER)
         val (a, b, c, d) = prefixAddress.split(DOT_DELIMITER)
         val ipAsNumber = fromIPGroupToNumber(IPGroup(a = a, b = b, c = c, d = d))
-        groupMask = (addresses - 2.0.pow(IPV4_BITS - suffix.toInt())).toLong()
-        ipAllowedGroup = fromNumberToIPGroup(applyMask(ipAsNumber, groupMask))
+        subnetMask = (addresses - 2.0.pow(IPV4_BITS - suffix.toInt())).toLong()
+        subnetAddress = fromNumberToIPGroup(applyMask(ipAsNumber, subnetMask))
 
     }
 
@@ -25,8 +26,8 @@ class IPFilter(cidrNotation: String) {
         val (a, b, c, d) = incomingIp.split(DOT_DELIMITER)
         val ipGroup = IPGroup(a = a, b = b, c = c, d = d)
         val incomingIpAsNum = fromIPGroupToNumber(ipGroup)
-        val incomingIPAfterMask = fromNumberToIPGroup(applyMask(incomingIpAsNum, groupMask))
-        return incomingIPAfterMask == ipAllowedGroup
+        val incomingIPAfterMask = fromNumberToIPGroup(applyMask(incomingIpAsNum, subnetMask))
+        return incomingIPAfterMask == subnetAddress
     }
 
     private fun applyMask(inputAddress: Long, mask: Long): Long {
@@ -53,13 +54,16 @@ class IPFilter(cidrNotation: String) {
 }
 
 fun main() {
-    val cidrNotation = "192.255.253.255/30"
-    val ipFilter = IPFilter(cidrNotation)
     val checkForAddressValid = "192.255.253.255"
     val checkForAddressInvalid = "192.255.253.220"
 
-    println("$cidrNotation - Configured as the filter")
-    println("$checkForAddressValid allowed: ${ipFilter.isAllowed(checkForAddressValid)}")
-    println("$checkForAddressInvalid allowed: ${ipFilter.isAllowed(checkForAddressInvalid)}")
+    val listOfCidrNotation = listOf("192.255.253.255/30", "192.255.253.255/25", "192.255.253.255/31")
+    val anyForFirst = listOfCidrNotation.any { IPFilter(it).isAllowed(checkForAddressValid) }
+    val anyForSecond = listOfCidrNotation.any { IPFilter(it).isAllowed(checkForAddressInvalid) }
+
+
+    println("${listOfCidrNotation.joinToString(", ")} - Configured as the filters")
+    println("$checkForAddressValid allowed: $anyForFirst")
+    println("$checkForAddressInvalid allowed: $anyForSecond")
 }
 
